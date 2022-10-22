300e2d2503c2104285b04985bad0d980a895301d
Michael F. Herbst <info@michael-herbst.com>
2022-10-22 17:36:54 +0200

    First version

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # ASEconvert
-
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://mfherbst.github.io/ASEconvert.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://mfherbst.github.io/ASEconvert.jl/dev/)
 [![Build Status](https://github.com/mfherbst/ASEconvert.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/mfherbst/ASEconvert.jl/actions/workflows/CI.yml?query=branch%3Amaster)
 [![Coverage](https://codecov.io/gh/mfherbst/ASEconvert.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/mfherbst/ASEconvert.jl)
+
+Light-weight module to install ASE
+and provide routines for converting between the Atoms datastructure
+from [ASE](https://wiki.fysik.dtu.dk/ase/index.html)
+and atomistic data provided in
+the [AtomsBase](https://github.com/JuliaMolSim/AtomsBase.jl) ecosystem.
+For both the package makes use of the
+[PythonCall](https://github.com/cjdoris/PythonCall.jl/).
+
+
+This can be used for example as follows
+```julia
+using ASEconvert
+using AtomsBase
+using DFTK
+
+# Make a silicon supercell using ASE
+atoms_ase = ase.build.bulk("Si") * (4, 1, 1)
+
+# Convert to an AtomsBase-compatible structure
+atoms_ab = pyconvert(AbstractSystem, atoms_ase)
+
+# Convert back to ASE and create a vacancy
+newatoms_ase = ase_atoms(atoms_ab)
+newatoms_ase.pop(4)
+```
--- src/ASEconvert.jl
+++ src/ASEconvert.jl
@@ -1,5 +1,66 @@
 module ASEconvert
+using PythonCall
+using AtomsBase
+using Unitful
 
-# Write your package code here.
+export ase_atoms
+export ase
+export pyconvert
+
+const ase = PythonCall.pynew()
+function __init__()
+    PythonCall.pycopy!(ase, pyimport("ase"))
+    PythonCall.pyconvert_add_rule("ase.atoms:Atoms", AbstractSystem, convert_ase)
+
+    # Make a bunch of submodules available
+    for sub in ("ase.io", "ase.build", "ase.lattice", "ase.visualize")
+        pyimport(sub)
+    end
+end
+
+function convert_ase(S::Type{<:AbstractSystem}, ase_atoms::Py)
+    box = [pyconvert(Vector, ase_atoms.cell[i])u"Å" for i = 0:2]
+
+    atnums     = pyconvert(Vector, ase_atoms.get_atomic_numbers())
+    positions  = pyconvert(Matrix, ase_atoms.get_positions())
+    velocities = pyconvert(Matrix, ase_atoms.get_velocities())
+    magmoms    = pyconvert(Vector, ase_atoms.get_initial_magnetic_moments())
+
+    atoms = [AtomsBase.Atom(atnums[i], positions[i, :]u"Å", velocities[i, :]u"Å/s";
+                            magnetic_moment=magmoms[i])
+             for i in 1:length(atnums)]
+
+    bcs = [p ? Periodic() : DirichletZero() for p in pyconvert(Vector, ase_atoms.pbc)]
+
+    # Parse extra data in info struct
+    info = pyconvert(Dict{Symbol, Any}, ase_atoms.info)
+    PythonCall.pyconvert_return(atomic_system(atoms, box, bcs; info...))
+end
+
+function ase_atoms(system::AbstractSystem{D}) where {D}
+    n_atoms = length(system)
+
+    cell = zeros(3, 3)
+    for i = 1:D
+        cell[i, 1:D] = ustrip.(uconvert.(u"Å", bounding_box(system, i)))
+    end
+
+    positions = zeros(n_atoms, 3)
+    for at in 1:n_atoms
+        positions[at, 1:D] = ustrip.(uconvert.(u"Å", position(system, at)))
+    end
+
+    velocities = nothing
+    if !ismissing(velocity(system))
+        velocities = zeros(n_atoms, 3)
+        for at in 1:n_atoms
+            velocities[at, 1:D] = ustrip.uconvert.(u"Å/s", velocity(system, at))
+        end
+    end
+
+    pbc = [boundary_conditions(system, i) == Periodic() for i = 1:D]
+    symbols = atomic_symbol(system)
+    ase.Atoms(; symbols, positions, cell, pbc, velocities)
+end
 
 end
